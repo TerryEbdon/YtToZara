8996f3d2106da3e163af4ccdeffb50e9b298eb32
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2024-01-29 18:19:52 +0000

    Remove UIDs from file names Fixes #17

--- YtToZara.cmd
+++ YtToZara.cmd
@@ -4,5 +4,6 @@ set tee=groovy YtToZara.groovy
 set ytcmd=\yt -q
 set audioFormat=--audio-format mp3
 set bitRate=--audio-quality 128K
+set fileNameOptions=--print filename --output "%%(title)s.%%(ext)s"
 set ytAudioOptions=-f 140 --extract-audio %audioFormat% %bitRate%
-%ytcmd% %ytAudioOptions% --print filename --no-simulate %* | %tee%
+%ytcmd% %ytAudioOptions% %fileNameOptions% --no-simulate %* | %tee%
