ef0a1387fceb9f9f04be2c9e1cdb55bb3e46204c
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2025-12-13 12:16:37 +0000

    Update yt-dlp from version v2025.11.12 to v2025.12.08 (#219)
    Fixes #209

--- src/main/groovy/net/ebdon/yttozara/Installer.groovy
+++ src/main/groovy/net/ebdon/yttozara/Installer.groovy
@@ -23,7 +23,7 @@ class Installer {
   static final String ffmpegFile    = "$downloadDir$ffmpegZip"
 
   static final String ytDlpRepo    = "$github/yt-dlp/yt-dlp"
-  static final String ytDlpVersion = '2025.11.12'
+  static final String ytDlpVersion = '2025.12.08'
   static final String ytDlpExe     = 'yt-dlp.exe'
   static final String ytDlpLatest  = "releases/download/${ytDlpVersion}"
   static final String ytDlpUrl     = "$ytDlpRepo/$ytDlpLatest/$ytDlpExe"
